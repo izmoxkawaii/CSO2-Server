93cf9e2e3a96ef941b363127788f6434581765a2
Yusuf <36341134+izmoxkawaii@users.noreply.github.com>
2020-12-12 21:34:46 +0300

    Can you do something like multi-language
    Can you do something like multi-language support for e-mail messages?

--- web/register/register.go
+++ web/register/register.go
@@ -29,8 +29,8 @@ var (
 		"",
 		"",
 		"",
-		"CSO2-Server",
-		"注册验证码",
+		"Counter-Strike Online 2 Verification Code",
+		"Save your Verification Code somewhere",
 		"",
 	}
 )
@@ -125,7 +125,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 			DebugInfo(2, Vcode)
 			Reglock.Lock()
 			MailService.TargetMail = addrtmp
-			MailService.Content = "您的验证码为：" + Vcode + "<br>" + "请勿告诉他人，如非本人操作请忽略本条邮件。请勿回复。"
+			MailService.Content = "Counter-Strike Online 2 Doğrulama Kodunuz<br>Your Counter-Strike Online 2 Verification Code<br>网上反恐精英2您的验证码<br>카운터-스트라이크 온라인2인증 코드  ：" + Vcode + "<br>" + ""
 			Reglock.Unlock()
 			if SendEmailTO(&MailService) != nil {
 				wth.Tip = MAIL_ERROR
